a3585417a23a5951c76beb6103ba605e1d29ab77
Dannybossboy <danielsmirnoff8@gmail.com>
2023-09-11 11:18:21 -0700

    init

---
+++ BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount {
+    public String accountNum;
+    public String createDate;
+    public int balance;
+    private String type;
+
+    public static String bankName;
+    private static int currentNumber = 0;
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
