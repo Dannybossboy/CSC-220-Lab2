e2efdaf651768a899775fd7a0d1e36735ff04414
Dannybossboy <danielsmirnoff8@gmail.com>
2023-09-11 12:57:31 -0700

    added stuff

--- BankAccount.java
+++ BankAccount.java
@@ -7,11 +7,41 @@ public class BankAccount {
     public static String bankName;
     private static int currentNumber = 0;
 
+    public BankAccount(String accountNum, String createDate) {
+        this.accountNum = accountNum;
+        this.createDate = createDate;
+    }
 
-
+    //Getters----------------
+    public String GetAccountNum() {
+        return accountNum;
+    }
+    public String GetCreateDate() {
+        return createDate;
+    }
+    public int GetBalance() {
+        return balance;
+    }
+    public String GetType() {
+        return type;
+    }
+    //Setters----------------
+    public void SetAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+    public void SetCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+    public void SetBalance(int balance) {
+        this.balance = balance;
+    }
+    public void SetType(String type) {
+        this.type = type;
+    }
 
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to your Bank Account");
+
     }
 
 }
